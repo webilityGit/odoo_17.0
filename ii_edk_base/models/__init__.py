5b9bd6ad515186d7d232043a39242b2df1783bd8
web_odoo <163117222+webilityGit@users.noreply.github.com>
2025-01-15 16:15:46 +0100

    Delete ii_edk_base directory

--- ii_edk_base/models/__init__.py
+++
@@ -1,9 +0,0 @@
-from . import document_category
-from . import document
-from . import document_archive
-from . import participants
-from . import  sef_service
-from . import config_parameters
-
-from . import document_location
-
