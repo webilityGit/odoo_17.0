e44a33507de697350a7de2d28f5867548b91f916
web_odoo <163117222+webilityGit@users.noreply.github.com>
2025-05-14 13:39:10 +0200

    Update __manifest__.py

--- account_payment_unece/__manifest__.py
+++ account_payment_unece/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "Account Payment UNECE",
-    "version": "17.0.1.0.0",
+    "version": "1.0",
     "category": "Accounting & Finance",
     "license": "AGPL-3",
     "summary": "UNECE nomenclature for the payment methods",
