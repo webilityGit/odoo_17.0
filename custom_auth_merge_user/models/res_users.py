b3cd4ed28983c2b841f221c81ca5b0639fad1072
web_odoo <163117222+webilityGit@users.noreply.github.com>
2025-07-16 12:35:17 +0200

    dodat modul za customer_auth_merge

---
+++ custom_auth_merge_user/models/res_users.py
@@ -0,0 +1,33 @@
+import logging
+from odoo import api, models
+
+_logger = logging.getLogger(__name__)
+
+class ResUsers(models.Model):
+    _inherit = 'res.users'
+
+    @api.model
+    def _signup_create_user(self, values):
+        """
+        Hook signup to merge duplicate partners based on email.
+        """
+        _logger.info('>>> appointment_auth_merge: signup hook values=%s', values)
+        user = super(ResUsers, self)._signup_create_user(values)
+
+        partner = user.partner_id
+        if partner and partner.email:
+            # find duplicates only by email
+            dup = self.env['res.partner'].sudo().search([
+                ('email','=', partner.email),
+                ('id','!=', partner.id),
+            ])
+            if dup:
+                # Create wizard with minimal default and then merge
+                wiz = self.env['base.partner.merge.automatic.wizard'].sudo().create({
+                    'dst_partner_id': partner.id,
+                })
+                wiz.write({'partner_ids': [(6, 0, dup.ids + [partner.id])]})
+                # perform merge
+                wiz.action_merge()
+
+        return user
