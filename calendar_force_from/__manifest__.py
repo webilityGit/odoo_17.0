c5fd4293bf3cb120ad7d70b0e5eb5b2cdd59641c
web_odoo <163117222+webilityGit@users.noreply.github.com>
2025-07-23 16:45:26 +0200

    dodat modul za test bookings@winera.com

---
+++ calendar_force_from/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    "name": "Calendar Event Force From",
+    "version": "1.0.1",
+    "summary": "Force 'From' on calendar invitations",
+    "author": "Vladimir",
+    "category": "Calendar",
+    "depends": ["mail"],
+    "data": [],
+    "installable": True,
+    "application": False,
+    "auto_install": False,
+}
