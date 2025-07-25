7f18c7edb1824caa8028e4d997104cffba93e00c
web_odoo <163117222+webilityGit@users.noreply.github.com>
2025-07-25 11:37:11 +0200

    ispavka modula za mailove za polje from

--- mail_from_calendar/__manifest__.py
+++ mail_from_calendar/__manifest__.py
@@ -2,9 +2,10 @@
     'name': 'Force From Address for Calendar Emails',
     'version': '18.0.1.0.0',
     'depends': ['mail', 'calendar'],
-    'author': 'Vladmir',
+    'author': 'Vladimir',
     'category': 'Tools',
     'summary': 'Force "From" address for calendar.attendee emails',
     'installable': True,
     'application': False,
+    'license': 'LGPL-3',
 }
--- mail_from_calendar/models/mail_mail.py
+++ mail_from_calendar/models/mail_mail.py
@@ -3,14 +3,18 @@ from odoo import models, api
 class MailMail(models.Model):
     _inherit = 'mail.mail'
 
-    @api.model
-    def create(self, vals):
-        if vals.get('model') == 'calendar.attendee' and not vals.get('email_from'):
-            # Pronađi šablon za calendar.attendee ako postoji i koristi njegov email_from
-            template = self.env['mail.template'].search([('model', '=', 'calendar.attendee'), ('email_from', '!=', False)], limit=1)
-            if template:
-                vals['email_from'] = template.email_from
-            else:
-                # Fallback adresa ako nema šablona
-                vals['email_from'] = 'Winera Bookings <bookings@winera.com>'
-        return super().create(vals)
+    @api.model_create_multi
+    def create(self, vals_list):
+        for vals in vals_list:
+            if vals.get('model') == 'calendar.attendee' and not vals.get('email_from'):
+                # Proveri da li postoji šablon za calendar.attendee
+                template = self.env['mail.template'].search([
+                    ('model', '=', 'calendar.attendee'),
+                    ('email_from', '!=', False)
+                ], limit=1)
+                if template:
+                    vals['email_from'] = template.email_from
+                else:
+                    vals['email_from'] = 'Winera Bookings <bookings@winera.com>'
+        return super().create(vals_list)
+
