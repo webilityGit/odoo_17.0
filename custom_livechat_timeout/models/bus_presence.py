3c00e17c3e8fcc3c1d1ce2f0c5967c8f36615eb5
web_odoo <163117222+webilityGit@users.noreply.github.com>
2025-08-01 11:01:25 +0200

    Delete custom_livechat_timeout directory

--- custom_livechat_timeout/models/bus_presence.py
+++
@@ -1,8 +0,0 @@
-from odoo import models
-
-class BusPresence(models.Model):
-    _inherit = 'bus.presence'
-
-    def _update_presence(self, inactivity_period=3600, identity_field='user_id', identity_value=None):
-        # Produženo sa 30 minuta (1800 sekundi) na 6 sati (21600 sekundi)
-        return super()._update_presence(inactivity_period=21600, identity_field=identity_field, identity_value=identity_value)
