e81ee0c51c3655d4e08ecbaaa2d2e9136d648ec5
web_odoo <163117222+webilityGit@users.noreply.github.com>
2025-08-01 10:04:46 +0200

    dodat modul za livechat, za neaktivnost

---
+++ custom_livechat_timeout/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    "name": "Custom Live Chat Presence Timeout",
+    "version": "1.0.0",
+    "category": "Customization",
+    "summary": "Extends user presence timeout to 4 hours for Live Chat operators.",
+    "author": "Vladimir",
+    "depends": ["bus", "im_livechat"],
+    "installable": True,
+    "application": False,
+    "auto_install": False,
+    "license": "LGPL-3",
+}
