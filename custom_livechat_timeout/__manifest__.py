254682c666fd2c613886a3929f703ae415fbf005
web_odoo <163117222+webilityGit@users.noreply.github.com>
2025-08-01 11:40:51 +0200

    ispravka modula za live chat

---
+++ custom_livechat_timeout/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    "name": "Custom Live Chat Notify",
+    "version": "1.0.0",
+    "author": "Vladimir",
+    "depends": ["im_livechat", "base_automation"],
+    "data": [
+        "data/automation_livechat_notify.xml",
+        "data/ir_cron_extend_livechat_presence.xml",
+    ],
+    "installable": True,
+    "auto_install": False,
+    "license": "LGPL-3",
+}
