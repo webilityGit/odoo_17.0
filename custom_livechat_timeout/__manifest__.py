0f61563408f1e1a2ec89583d845f2cd6a15f3778
web_odoo <163117222+webilityGit@users.noreply.github.com>
2025-08-01 11:25:30 +0200

    nova ispravka live chat

---
+++ custom_livechat_timeout/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "Livechat Extended Timeout & Notification",
+    "version": "1.0.0",
+    "category": "Chat",
+    "author": "Vladimir",
+    "summary": "Notify Livechat Operators even if inactive",
+    "depends": ["bus", "im_livechat", "mail"],
+    "data": [
+        "data/automation_livechat_notify.xml",
+    ],
+    "installable": True,
+    "application": False,
+    "license": "LGPL-3",
+}
